f5f6e24a7a40f67be8bbfb60dc90108c4c1a11b7
minhaj123456 <133559085+minhaj123456@users.noreply.github.com>
2023-08-24 12:35:56 +0530

    hyy

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MainScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: Center(
+      
+        child: Text("splash screen"),
+      ),
+      backgroundColor: Colors.brown,
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome to the Main Screen!'),
+      ),
+    );
+  }
+}
---
+++ lib/splash.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class secondclss extends StatelessWidget {
+  const secondclss({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
